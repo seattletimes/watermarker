3a99e6bdb32dbd5ad98a6d5af361d10cc2b13edc
Thomas Wilburn <news@thomaswilburn.net>
2015-01-09 13:43:18 -0800

    First

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  var less = require("less");
+  var fs = require("fs");
+
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.initConfig({
+    watch: {
+      main: {
+        files: ["src/*.*"],
+        tasks: ["default"],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  })
+
+  grunt.registerTask("default", function() {
+    var c = this.async();
+
+    var index = fs.readFileSync("src/index.html", { encoding: "utf8" });
+    var style = fs.readFileSync("src/styles.less", { encoding: "utf8" });
+
+    less.render(style, function(err, css) {
+      var output = grunt.template.process(index, { 
+        data: {
+          style: css,
+          script: fs.readFileSync("src/script.js", { encoding: "utf8" }),
+          light: fs.readFileSync("src/logo-light.png").toString("base64"),
+          dark: fs.readFileSync("src/logo-dark.png").toString("base64")
+        }
+      });
+
+      fs.writeFileSync("watermark.html", output);
+      c();
+    });
+    
+  });
+
+};
---
+++ src/script.js
@@ -0,0 +1,95 @@
+//dom patching
+var queryAll = function(selector) {
+  return Array.prototype.slice.call(document.querySelectorAll(selector));
+};
+var query = document.querySelector.bind(document);
+
+//globals
+var download = query(".download-button");
+var canvas = query("canvas");
+var context = canvas.getContext("2d");
+context.imageSmoothingEnabled = true;
+var config = {
+  markLocation: { x: 10, y: 10 }
+};
+var light = new Image();
+light.src = "data:image/png;base64,<%= light %>";
+var dark = new Image();
+dark.src = "data:image/png;base64,<%= dark %>";
+var sourceImage;
+
+//updates
+var updateConfig = function() {
+  config.limit = query(".limit").value;
+  config.contrast = query("[name=contrast]:checked").value;
+};
+
+var render = function() {
+  if (!sourceImage) return;
+  if (sourceImage.width > sourceImage.height) {
+    canvas.width = config.limit;
+    var height = sourceImage.height / sourceImage.width;
+    canvas.height = config.limit * height;
+  } else {
+    canvas.height = config.limit;
+    var width = sourceImage.width / sourceImage.height;
+    canvas.width = config.limit * width;
+  }
+  context.drawImage(sourceImage, 0, 0, canvas.width, canvas.height);
+  var watermark = config.contrast == "light" ? light : dark;
+  context.drawImage(watermark, config.markLocation.x, config.markLocation.y);
+  var data = canvas.toDataURL("image/jpeg");
+  download.setAttribute("href", data);
+};
+
+//listeners
+var cancel = function(e) { e.preventDefault() };
+
+var onDrop = function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  var file = e.dataTransfer.files[0];
+  var reader = new FileReader();
+  reader.onload = function() {
+    sourceImage = new Image();
+    sourceImage.src = reader.result;
+    sourceImage.onload = render;
+  };
+  reader.readAsDataURL(file);
+};
+
+document.body.addEventListener("dragover", cancel);
+document.body.addEventListener("drop", onDrop);
+
+var onClickCanvas = function(e) {
+  if (e.target !== canvas) return;
+  var canvasCoords = canvas.getBoundingClientRect();
+  var adjusted = {
+    x: e.pageX - canvasCoords.left,
+    y: e.pageY - canvasCoords.top
+  };
+  if (adjusted.x > canvas.width / 2) {
+    config.markLocation.x = canvas.width - 10 - light.width;
+  } else {
+    config.markLocation.x = 10;
+  }
+  if (adjusted.y > canvas.height / 2) {
+    config.markLocation.y = canvas.height - 10 - light.height;
+  } else {
+    config.markLocation.y = 10;
+  }
+  render();
+};
+
+document.body.addEventListener("click", onClickCanvas);
+
+var onChange = function() {
+  updateConfig();
+  render();
+};
+
+document.body.addEventListener("change", onChange);
+document.body.addEventListener("keyup", onChange);
+
+//kickoff
+updateConfig();
